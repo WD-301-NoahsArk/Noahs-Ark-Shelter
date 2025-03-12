d64e5c733cc302ab7f68d093d7dbbb2ca2389f6d
Kurt <kurtchristiansanchez@gmail.com>
2025-03-12 23:00:28 +0800

    Fixed Adoption's Page

--- noahs-client/src/app/page/contact/contact.component.ts
+++ noahs-client/src/app/page/contact/contact.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  standalone: true,
+  imports: [FormsModule]
 })
 export class ContactComponent {
+  @Output() close = new EventEmitter<void>();
 
+  isModalOpen = false;
+
+  formData = {
+    name: '',
+    email: '',
+    message: ''
+  };
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.close.emit(); // Notify parent to close modal if needed
+  }
+
+  onSubmit() {
+    console.log('Form submitted:', this.formData);
+    this.closeModal(); // Close the modal after submission
+  }
 }
--- noahs-client/tailwind.config.js
+++ noahs-client/tailwind.config.js
@@ -1,18 +1,22 @@
- /** @type {import('tailwindcss').Config} */
- export default {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         maincolor: {
-          100: "#CFBF72",
-          200: "#D16A54",
-          300: "#cbe3fd",
-          400: "#90cdfd",
-          500: "#63b3ed",
-
+          highlight2: "#653A36",
+          highlight: "#D16A54",
+          highlight3: "#EAAD62",
+          primary: "#E8D4A5",
+          secondary: "#CFBF72",
+          sectionsdark: "#7A7D55",
+          sections: "#314A4A",
+          whitetext: "#E3E3E3",
+          blacktext: "#212121",
+        },
+      },
     },
   },
-
   plugins: [],
-}}}
+}
