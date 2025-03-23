9bd6b4270ed2cee29c07b11affd9a3a511a04a87
JullianMQ <jullianmq@gmail.com>
2025-03-24 00:28:16 +0800

    added login func to navbar

--- noahs-client/src/app/component/navbar/navbar.component.ts
+++ noahs-client/src/app/component/navbar/navbar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpService } from '../../http.service';
+import { AuthService } from '../../auth.service';
+
 @Component({
   selector: 'app-navbar',
   imports: [RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
+  providers: [HttpService, AuthService]
 })
 export class NavbarComponent {
+  isLoggedIn: boolean = false
+
+  constructor(private http: HttpService, private authService: AuthService) { }
 
 }
