dfbab0db3399d01c71276fb1609532755ed182c5
JullianMQ <jullianmq@gmail.com>
2025-03-24 00:24:27 +0800

    modified login for admin

--- noahs-client/src/app/pages/login/login.component.ts
+++ noahs-client/src/app/pages/login/login.component.ts
@@ -1,43 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpService } from '../../http.service';
 import { ButtonComponent } from '../../component/button/button.component';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [
     CommonModule,
+    ButtonComponent,
+    ReactiveFormsModule,
     ButtonComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   providers: [HttpService]
 })
-export class LoginComponent {
-  token = ""
-  getLoginCredentials() {
-    let htmlEmail = document.querySelector("#email") as HTMLInputElement;
-    let htmlPass = document.querySelector("#user_pass") as HTMLInputElement;
-    let email = htmlEmail.value;
-    let pass = htmlPass.value;
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup
+  isLoggedIn = false
+  errorMessage: string = ''
 
-    htmlEmail.value = "";
-    htmlPass.value = "";
-    return { userEmail: email, userPass: pass };
+  constructor(private http: HttpService, private authService: AuthService, private router: Router) {
+    this.loginForm = new FormGroup({
+      email: new FormControl(''),
+      password: new FormControl('')
+    });
   }
 
-  constructor(private http: HttpService) { }
+  ngOnInit(): void {
+    this.authService.authStatus$.subscribe(status => {
+      this.isLoggedIn = status;
 
-  // janedoe@example.com
-  // secpass
+      if (status) {
+        this.router.navigate(['/admin/adoption']);
+        return
+      }
+    });
+  }
+
   loginFunc() {
-  let { userEmail, userPass } = this.getLoginCredentials();
-    this.http.loginAdmin(userEmail, userPass).subscribe(res => {
-      this.token = res.token;
-      this.http.goAdmin(this.token).subscribe(res => {
-        console.log(res.message)
-      });
-    })
+    const { email, password } = this.loginForm.value;
+
+    if (!email || !password) {
+      this.errorMessage = "Email and password are required.";
+      return;
+    }
+
+    this.http.loginAdmin(email, password).subscribe({
+      next: (res) => {
+        this.authService.login(res.token);
+        console.log("Login successful!");
+      },
+      error: (err) => {
+        this.errorMessage = "Invalid email or password.";
+        console.error("Login failed:", err);
+      }
+    });
+
+    this.loginForm.reset();
   }
 
+  adoptFunc = () => {
+    window.location.href = '/adoption'
+  }
 }
+
